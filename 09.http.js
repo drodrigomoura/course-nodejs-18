50912586322b1443bcab8ae57742d07294032d8e
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2026-02-11 11:37:54 -0300

    http server

--- 09.http.js
+++ 09.http.js
@@ -1,10 +1,17 @@
 const http = require('node:http')
+const { findAvailablePort } = require('./10-free-port')
+
+const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   console.log('Request received!')
   res.end('Hello World!')
 })
 
-server.listen(0, () => {
-  console.log(`Server listening on port http://localhost:${server.address().port}`)
+findAvailablePort(port).then((port) => {
+  server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+  })
+}).catch((err) => {
+  console.error('Error finding available port', err)
 })
