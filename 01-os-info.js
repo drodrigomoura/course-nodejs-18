9e1037a81690034b3502f645550c431df3190ad9
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2026-02-11 11:17:23 -0300

    os info

---
+++ 01-os-info.js
@@ -0,0 +1,10 @@
+const os = require('node:os')
+
+console.log(os.platform())
+console.log(os.release())
+console.log(os.arch())
+console.log(os.cpus())
+console.log(os.cpus().length)
+console.log(os.totalmem() / 1024 / 1024)
+console.log(os.freemem() / 1024 / 1024)
+console.log(os.uptime() / 60 / 60 / 24)
