df1d59e1755c7a55c41367797fc67d34453be855
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2026-02-13 09:25:09 -0300

    express api rest - cors

--- 03-api-rest/app.js
+++ 03-api-rest/app.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const crypto = require('node:crypto')
+const cors = require('cors')
 const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.disable('x-powered-by')
-
 app.use(express.json())
+
+const ACCEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'https://my-movies-app.com',
+  'https://movies.com'
+]
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}))
 
 // Get all or using query parameters
 app.get('/api/v1/movies', (req, res) => {
@@ -78,6 +94,38 @@ app.patch('/api/v1/movies/:id', (req, res) => {
   movies[movieIndex] = updatedMovie
 
   res.json(updatedMovie)
+})
+
+// CORS
+// normal methods: GET/HEAD/POST
+// preflight(complex) methods: PUT/PATCH/DELETE
+
+// CORS preflight request handler
+// Options requests are sent by the browser before complex requests to check if the server allows the actual request
+
+// app.options('/api/v1/movies/:id', (req, res) => {
+//   const origin = req.headers.origin
+
+//   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//     res.header('Access-Control-Allow-Origin', origin)
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+//   }
+
+//   res.sendStatus(204)
+// })
+
+// DELETE movie
+app.delete('/api/v1/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  res.json({ message: 'Movie deleted' })
 })
 
 const PORT = process.env.PORT || 3000
